13ef1905614d13c8131f0fbd4086ddf5fd60fea2
Thisara Dilshan Hewage <thisarad582@gmail.com>
2023-02-27 18:41:27 +0530

    add files initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def AddNumbers(n1,n2):
+    return n1+n2
+if __name__ == '__main__':
+    number1=23
+    number2 =45
+    res=AddNumbers(number1,number2)
+    print(res)
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import *
+
+def Test_add():
+    assert AddNumbers(20,30)==50
