3972eb7d75c05a9e394342ae00ff58e3565746a5
Thisara Dilshan Hewage <thisarad582@gmail.com>
2023-02-27 22:17:48 +0530

    added new function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pytest
 def AddNumbers(n1,n2):
     return n1+n2
+def SubtractNumbers(n1,n2):
+    return n1-n2
 if __name__ == '__main__':
     number1=23
     number2 =45
     res=AddNumbers(number1,number2)
+    res2=SubtractNumbers(number2-number1)
     print(res)
+    print(res2)
