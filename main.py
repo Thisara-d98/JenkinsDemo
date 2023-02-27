4bdb56edc1819b285c235ed3ba91192957cfa047
Thisara Dilshan Hewage <thisarad582@gmail.com>
2023-02-27 22:19:40 +0530

    added new function

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     number1=23
     number2 =45
     res=AddNumbers(number1,number2)
-    res2=SubtractNumbers(number2-number1)
+    res2=SubtractNumbers(number2,number1)
     print(res)
     print(res2)
